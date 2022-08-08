759e9d32c5d7177a65f63a9012add77961012e88
Shea Meyers <shea.meyers@gmail.com>
2022-08-08 20:05:21 +0200

    Display job post on web page

---
+++ ProgrammingExperienceJobsApp/Pages/JobPosts/Index.cshtml.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ProgrammingExperienceJobsApp.Data;
+using ProgrammingExperienceJobsApp.Models;
+
+namespace ProgrammingExperienceJobsApp.Pages.JobPosts;
+public class IndexModel : PageModel
+{
+    private readonly ApplicationDbContext _db;
+
+    public IEnumerable<JobPost> JobPosts { get; set; }
+
+    public IndexModel(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+
+    public void OnGet()
+    {
+        JobPosts = _db.JobPost;
+    }
+}
