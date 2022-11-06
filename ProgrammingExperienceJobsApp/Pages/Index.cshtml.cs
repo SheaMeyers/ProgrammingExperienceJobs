c52d0fefbacba9df90e9d7d5e1ad6dfc5894f4dc
Shea Meyers <shea.meyers@gmail.com>
2022-11-06 18:43:39 +0100

    Add filtering jobs by experience and years

--- ProgrammingExperienceJobsApp/Pages/Index.cshtml.cs
+++ ProgrammingExperienceJobsApp/Pages/Index.cshtml.cs
@@ -1,4 +1,5 @@
 using System.Threading.Tasks.Dataflow;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using ProgrammingExperienceJobsApp.Constants;
 using ProgrammingExperienceJobsApp.Data;
@@ -10,15 +11,26 @@ public class IndexModel : PageModel
     private readonly ApplicationDbContext _db;
 
     public IEnumerable<JobPost> JobPosts { get; set; }
+    [BindProperty]
+    public Experience Experience { get; set; }
+    public Array languageValues { get; set; }
 
     public IndexModel(ApplicationDbContext db)
     {
         _db = db;
     }
 
+    public void OnPost()
+    {
+        languageValues = Enum.GetValues(typeof(Languages));
+
+        JobPosts = _db.JobPost.Where(jp => jp.Experiences.Contains(Experience)).OrderByDescending(jp => jp.CreatedDate);
+    }
+
     public void OnGet()
     {
-        var experience = _db.Experience.First(e => e.Language == Languages.CSharp);
-        JobPosts = _db.JobPost.Where(jp => jp.Experiences.Contains(experience)).OrderByDescending(jp => jp.CreatedDate);
+        languageValues = Enum.GetValues(typeof(Languages));
+
+        JobPosts = _db.JobPost;
     }
 }
